2284706c7dd98960fa4fbcee91bfd6362a9665d1
kmy1 <98854090+kmly2@users.noreply.github.com>
2026-02-20 15:09:27 -0500

    Add hour counter functionality in contador.js

---
+++ ejercicios_js_basicos/contador.js
@@ -0,0 +1,9 @@
+let horasTrabajadas = 0;
+
+function incrementarHoras(horas) {
+  horasTrabajadas += horas;
+  return horasTrabajadas;
+}
+
+console.log(incrementarHoras(2)); // 2
+console.log(incrementarHoras(3)); // 5
